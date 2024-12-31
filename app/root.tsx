9d0ff803dedbf802d3329bdf732a72d0fdcf59c9
FARUK2059 <omarfaruk34552@gmail.com>
2024-12-31 21:00:54 +0600

    add store

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import {
 
 import type { Route } from "./+types/root";
 import stylesheet from "./app.css?url";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -34,7 +36,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <Provider store={store}>
+          {children}
+        </Provider>
         <ScrollRestoration />
         <Scripts />
       </body>
