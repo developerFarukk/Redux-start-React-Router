f66c14be123f86d42562862b7ae8b3bafe60d43a
FARUK2059 <omarfaruk34552@gmail.com>
2025-01-04 00:06:43 +0600

    add in den fn add

--- app/components/main/Main.tsx
+++ app/components/main/Main.tsx
@@ -1,6 +1,24 @@
 
+import { useDispatch } from "react-redux";
+import { decrement, increment } from "~/redux/features/counterSlice";
+
 
 const Main = () => {
+
+const dispatch = useDispatch();
+
+
+
+const handleIncrement = () => {
+    dispatch(increment())
+    
+}
+
+
+const handleDecrement = () => {
+    dispatch(decrement())
+}
+
     return (
         <main className="flex items-center justify-center pt-16 pb-4">
             <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
@@ -12,14 +30,13 @@ const Main = () => {
                             <h1 className="text-2xl text-center m-4">Counter With Redux</h1>
 
                             <div className="flex gap-6 items-center">
-                                <button className="btn bg-yellow-300 text-2xl text-blue-950 p-2 font-bold m-2 rounded-2xl">Increment</button>
+                                <button onClick={handleIncrement} className="btn bg-yellow-300 text-2xl text-blue-950 p-2 font-bold m-2 rounded-2xl">Increment</button>
 
                                 <div className="text-3xl"> 0 </div>
 
-                                <button className="bg-red-900 text-2xl text-white p-2 font-bold m-2 rounded-2xl" > Decrement </button>
+                                <button onClick={handleDecrement} className="bg-red-900 text-2xl text-white p-2 font-bold m-2 rounded-2xl" > Decrement </button>
 
                             </div>
-                            <p>ddddddddd</p>
                         </div>
 
 
