499f664db1a8807b9619373ab94120950b091f58
FARUK2059 <omarfaruk34552@gmail.com>
2025-01-03 23:14:51 +0600

    add CounterSlice

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,5 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
-export default [index("components/main/Main.tsx")] satisfies RouteConfig;
+export default [
+    index("components/main/Main.tsx")
+] satisfies RouteConfig;
